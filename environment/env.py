dc918abb3d1b1306ab45ed69f62a84609f4fcbab
Marwin34 <marcinwojsik@wp.pl>
2020-05-05 16:11:46 +0200

    Fix environment update

--- environment/env.py
+++ environment/env.py
@@ -6,8 +6,13 @@ from gym import error, spaces, utils
 import traci
 import traci.constants as tc
 
+import imageio
+from os import listdir
+from os.path import isfile, join
 import os, sys
+import tempfile
 import numpy as np
+import datetime
 
 from settings import PROJECT_ROOT
 
@@ -20,6 +25,8 @@ DIM_H = int(NET_HEIGHT / VEHICLE_LENGTH)
 
 TRAFFICLIGHTS_PHASES = 4
 
+REPLAY_FPS = 30
+
 if "SUMO_HOME" in os.environ:
     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
     sys.path.append(tools)
@@ -30,7 +37,13 @@ else:
 class SumoEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
 
-    def __init__(self, config_file=Path(PROJECT_ROOT, "environment", "2lane.sumocfg"), render=False):
+    def __init__(
+        self,
+        config_file=Path(PROJECT_ROOT, "environment", "2lane.sumocfg"),
+        replay_folder=Path(PROJECT_ROOT, "replays"),
+        save_replay=False,
+        render=False,
+    ):
         super().__init__()
 
         sumo_binary = ""
@@ -58,6 +71,10 @@ class SumoEnv(gym.Env):
         self.phases_durations = [20.0, 20.0, 20.0, 20.0]
 
         self.last_wait_time = 0
+
+        self.save_replay = save_replay
+        self.temp_folder = tempfile.TemporaryDirectory()
+        self.replay_folder = replay_folder
 
     def step(self, action):
         reward = None
@@ -99,18 +116,22 @@ class SumoEnv(gym.Env):
         phases_tested_cnt = 0
         prev_phase = -1
         while traci.simulation.getMinExpectedNumber() > 0:
-            if phases_tested_cnt == TRAFFICLIGHTS_PHASES * 2:
+            current_phase = traci.trafficlight.getPhase(self.tls_id)
+
+            if prev_phase != current_phase:
+                phases_tested_cnt += 1
+
+            if phases_tested_cnt == TRAFFICLIGHTS_PHASES * 2 + 1:
                 break
 
-            current_phase = traci.trafficlight.getPhase(self.tls_id)
             if current_phase % 2 == 0 and current_phase != prev_phase:
                 phase_id = int(current_phase / 2)
                 if current_phase == action_tuple[0]:
                     self.phases_durations[phase_id] += action_tuple[1]
 
                     if (
-                            self.phases_durations[phase_id] < 0
-                            or self.phases_durations[phase_id] > 60
+                        self.phases_durations[phase_id] < 0
+                        or self.phases_durations[phase_id] > 60
                     ):
                         not_viable_action_penalty = -1000
 
@@ -121,9 +142,6 @@ class SumoEnv(gym.Env):
                 traci.trafficlight.setPhaseDuration(
                     self.tls_id, self.phases_durations[phase_id]
                 )
-
-            if prev_phase != current_phase:
-                phases_tested_cnt += 1
 
             prev_phase = current_phase
             traci.simulationStep()
@@ -139,6 +157,12 @@ class SumoEnv(gym.Env):
                     ]
 
                     wait_time_map[vehicle] = vehicle_wait_time
+
+                if self.save_replay:
+                    time = traci.simulation.getTime()
+                    traci.gui.screenshot(
+                        "View #0", self.temp_folder.name + f"/state_{time}.png"
+                    )
 
         wait_time_sum = 0
         for entry in wait_time_map:
@@ -162,5 +186,25 @@ class SumoEnv(gym.Env):
     def save_simulation(self, path="sim_res.sbx"):  # for future usage
         traci.simulation.saveState(path)
 
+    def _generate_gif(self):
+        src_path = self.temp_folder.name
+        res_path = self.replay_folder
+
+        onlyfiles = [f for f in listdir(src_path) if isfile(join(src_path, f))]
+        filenames = [f for f in onlyfiles if ".png" in f]
+
+        res_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        with imageio.get_writer(
+            f"{res_path}/{res_name}_sim.gif", mode="I", fps=REPLAY_FPS
+        ) as writer:
+            for filename in filenames:
+                image = imageio.imread(f"{src_path}/{filename}")
+                writer.append_data(image)
+
     def close(self):
         traci.close()
+
+        if self.save_replay:
+            self._generate_gif()
+
+            self.temp_folder.cleanup()
