1268256048cf4c818b5c7546347871c4860942b0
kaspersapala <kasper.sapala@gmail.com>
2020-06-17 19:18:36 +0200

    update collab init

--- collab_init.sh
+++ collab_init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# chmod 777 collab_init.sh
+
 sudo add-apt-repository ppa:sumo/stable -y
 sudo apt-get update
 sudo apt-get install sumo sumo-tools sumo-doc
 
+
 pip install traci
 pip install gym
+pip install pygame
 
+# %env  SUMO_HOME=/usr/local/share/sumo
 export SUMO_HOME=/usr/local/share/sumo
