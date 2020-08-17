44e3965b7cb5f91f3ff7b7b2e59a909f86270ee3
kaspersapala <kasper.sapala@gmail.com>
2020-08-17 17:34:11 +0200

    reformat training_aaai.py

--- training_aaai.py
+++ training_aaai.py
@@ -6,36 +6,6 @@ with AaaiEnv(render=True) as env:
     sleep(10)
 
     for i in range(8):
-        print("zmieniam na {}".format(i))
+        print("changing to {}".format(i))
+        print('throughput: {}, travel_time: {}'.format(env.get_throughput(), env.get_travel_time()))
         env.step(i)
-
-    #
-    # print("zmieniam na 0")
-    # print(env.step(0))
-    # print("zmieniam na 0")
-    # print(env.step(0))
-    # print("zmieniam na 1")
-    # print(env.step(1))
-    # print("zmieniam na 1")
-    # print(env.step(1))
-    # print('throughput: {}, travel_time: {}'.format(env.get_throughput(), env.get_travel_time()))
-    # print("zmieniam na 2")
-    # print(env.step(2))
-    # print("zmieniam na 2")
-    # print(env.step(2))
-    # print('throughput: {}, travel_time: {}'.format(env.get_throughput(), env.get_travel_time()))
-    # print("zmieniam na 3")
-    # print(env.step(3))
-    # print("zmieniam na 3")
-    # print(env.step(3))
-    # print("zmieniam na 0")
-    # print(env.step(0))
-    # print("zmieniam na 0")
-    # print(env.step(0))
-    # print("zmieniam na 2")
-    # print(env.step(2))
-    #
-    # print('throughput: {}, travel_time: {}'.format(env.get_throughput(), env.get_travel_time()))
-    #
-    # for i in range(5):
-    #     print(env.step(0))
