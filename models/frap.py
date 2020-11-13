8280ffb8cdb499e3af47185e3c0367c302ec6f97
Marcin Wójsik <marcinwojsik@wp.pl>
2020-11-13 19:43:13 +0100

    Feature/torchscriptcompatibility (#14)
    * adapt python script to work with jit.scirpt()
    
    * repair error in for loops
    
    * cleanup
    
    Co-authored-by: kaspersapala <kasper.sapala@gmail.com>

--- models/frap.py
+++ models/frap.py
@@ -5,23 +5,6 @@ import torch
 from models.neural_net import SerializableModel
 from rlpyt.utils.tensor import infer_leading_dims, restore_leading_dims
 
-NUM_PHASES = 8
-# device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-
-# from what traffic movements phases are made of
-phases_movements = {
-    0: (10, 4),
-    1: (5, 4),
-    2: (10, 11),
-    3: (5, 11),
-    4: (1, 7),
-    5: (8, 7),
-    6: (1, 2),
-    7: (8, 2)
-}
-
-worth_movements = {1, 2, 4, 5, 7, 8, 10, 11}
-
 
 class Frap(SerializableModel):
 
@@ -86,6 +69,23 @@ class Frap(SerializableModel):
 
         self.last_conv = nn.Conv2d(conv_channels_size, 1, 1)
 
+        # CONSTANTS
+        self.worth_movements = [1, 2, 4, 5, 7, 8, 10, 11]
+
+        self.num_phases = 8
+
+        # from what traffic movements phases are made of
+        self.phases_movements = {
+            0: (10, 4),
+            1: (5, 4),
+            2: (10, 11),
+            3: (5, 11),
+            4: (1, 7),
+            5: (8, 7),
+            6: (1, 2),
+            7: (8, 2)
+        }
+
     def _calc_deamand(self, curr_phases, pressure):
         h_v = F.relu(self.phase_v(pressure))
         h_s = F.relu(self.phase_s(curr_phases))
@@ -97,23 +97,27 @@ class Frap(SerializableModel):
         curr_phases = pressures[:, 0].unsqueeze(-1)
         phases_pressures = pressures[:, 1:]
         saved_demands = dict()
-        for m in worth_movements:
+        for m in self.worth_movements:
             my_d = self._calc_deamand(curr_phases, phases_pressures[:, m].unsqueeze(-1))
-            saved_demands[m] = my_d
+            saved_demands[str(m)] = my_d
         phase_demands = []
-        for i in range(NUM_PHASES):
-            m_a, m_b = phases_movements[i]
-            phase_demand = saved_demands[m_a] + saved_demands[m_b]
+        for i in range(self.num_phases):
+            m_a, m_b = self.phases_movements[i]
+            phase_demand = saved_demands[str(m_a)] + saved_demands[str(m_b)]
             phase_demands.append(phase_demand)
 
-        demand_embedding = torch.cat([
-            torch.cat([
-                torch.cat([phase_demands[p], phase_demands[q]], dim=1).unsqueeze(1).unsqueeze(1)
-                for q in range(NUM_PHASES) if q != p
-            ], dim=2)
-            for p in range(NUM_PHASES)
-        ], dim=1)
+        tmp = []
+        for p in range(self.num_phases):
+            tmp2 = []
+            for q in range(self.num_phases):
+                if q != p:
+                    tmp2.append(torch.cat([phase_demands[p], phase_demands[q]], dim=1).unsqueeze(1).unsqueeze(1))
+
+            tmp.append(torch.cat(tmp2, dim=2))
+
+        demand_embedding = torch.cat(tmp, dim=1)
         relation_embedding = self.rel_embedding(self.competing_matrix).unsqueeze(0)
+
         relation_conv_out = self.relation_conv(relation_embedding.permute(0, 3, 1, 2).contiguous())
         demand_conv_out = self.demand_conv(demand_embedding.permute(0, 3, 1, 2).contiguous())
 
