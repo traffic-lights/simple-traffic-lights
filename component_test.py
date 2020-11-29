6c06e50f6790a00dd92e7ca7f3b1eba232d17525
Marwin34 <marcinwojsik@wp.pl>
2020-11-29 11:43:28 +0100

    change settings file, repair backoff

--- component_test.py
+++ component_test.py
@@ -12,14 +12,14 @@ from settings import PROJECT_ROOT, JSONS_FOLDER
 REPLY_TO = "amq.rabbitmq.reply-to"
 
 parser = argparse.ArgumentParser("app to test component")
-parser.add_argument("target")
-parser.add_argument("--max_steps", type=int, default=-1)
-parser.add_argument("--no_render", default=True, action="store_false")
+parser.add_argument("settings", help="client settings file name")
+parser.add_argument("--max_steps", type=int, default=-1, help="simulation max steps, -1==infinite")
+parser.add_argument("--no_render", default=True, action="store_false", help="render mode, default True")
 
 
-class Target:
+class Settings:
     def __init__(self, file_name):
-        with open(file_name) as json_file:
+        with open(Path(PROJECT_ROOT, 'tester_settings', file_name)) as json_file:
             data = json.load(json_file)
 
             self.address = data["address"]
@@ -27,6 +27,7 @@ class Target:
             self.requests_queue = data["requests_queue"]
             self.user = data["user"]
             self.password = data["password"]
+            self.env_file = data["env"]
 
 
 def publish(channel, state, routing):
@@ -63,19 +64,19 @@ def callback(env, publish_queue, ch, method, properties, body):
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    target_file = args.target
+    settings_file = args.settings
     max_steps = args.max_steps
     render = args.no_render
 
-    target = Target(target_file)
+    settings = Settings(settings_file)
 
-    credentials = pika.PlainCredentials(target.user, target.password)
+    credentials = pika.PlainCredentials(settings.user, settings.password)
 
     env = SumoEnv.from_config_file(
-        Path(JSONS_FOLDER, 'configs', 'aaai.json'), max_steps).create_runner(render=render)
+        Path(JSONS_FOLDER, 'configs', settings.env_file), max_steps).create_runner(render=render)
 
     state = env.reset()
-    callback = partial(callback, env, target.requests_queue)
+    callback = partial(callback, env, settings.requests_queue)
 
     retries = 0
     while True:
@@ -85,15 +86,14 @@ if __name__ == "__main__":
         backoff = 0.05 * math.exp(retries)
         try:
             connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=target.address, port=5672, virtual_host=target.vhost, credentials=credentials))
+                pika.ConnectionParameters(host=settings.address, port=5672, virtual_host=settings.vhost, credentials=credentials))
             channel = connection.channel()
 
             channel.basic_consume(REPLY_TO, callback, True)
 
-            publish(channel, state, target.requests_queue)
+            publish(channel, state, settings.requests_queue)
             retries = 0
         except Exception as e:
-            print(f"DEBUG: {e}")
             print("Unable to connect. Retrying ...")
             sleep(backoff)
             retries += 1
@@ -104,6 +104,7 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"DEBUG: {e}")
             print("Connection closed. Reconnecting ...")
+            backoff = 0.05 * math.exp(retries)
             sleep(backoff)
             retries += 1
             continue
